c3ae5fcb0418ae199cef8ba237de6a995c7e9f67
Mast3r_waf1z <thomasmollerjensen@live.dk>
2022-11-28 13:24:10 +0100

    added base shell

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
+mkdir -v files
+if test -f "files/config.conf"; then
+    echo "file 'files/config.conf' already exists"
+else
+    echo "initializing file 'files/config.conf'"
+    echo "token=" >> files/config.conf
+fi
+echo "compiling program..."
 mvn clean compile package -q -f nutbot
