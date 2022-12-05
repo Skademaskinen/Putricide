7a4f127bfdb0356e5d67e3a29bfaab4115be94d0
mast3r_waf1z <thomasmollerjensen@live.dk>
2022-12-05 19:24:30 +0100

    finished enhancement

--- nutbot/src/main/java/skademaskinen/Commands/Pvp.java
+++ nutbot/src/main/java/skademaskinen/Commands/Pvp.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.MessageEmbed.Field;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
@@ -27,124 +26,34 @@ import skademaskinen.Bot;
 import skademaskinen.Utils.Utils;
 import skademaskinen.WorldOfWarcraft.BattleNetAPI;
 import skademaskinen.WorldOfWarcraft.Character;
-import skademaskinen.WorldOfWarcraft.RaidTeam;
+import skademaskinen.WorldOfWarcraft.PvpTeam;
 
-public class Raid implements Command {
-    protected boolean success = false;
-    private boolean shouldEphemeral = true;
-    protected List<ActionRow> actionRows = new ArrayList<>();
-    private boolean defer = true;
-
+public class Pvp extends Raid {
+    
     public static CommandData configure(){
-        SlashCommandData command = Commands.slash(Raid.class.getSimpleName().toLowerCase(), "Admin command: Handle the raid team");
-        SubcommandData add = new SubcommandData("add", "Add a raider to the raid team manually");
-        OptionData raider = new OptionData(OptionType.USER, "raider", "Mention of the raider", true);
+        SlashCommandData command = Commands.slash(Pvp.class.getSimpleName().toLowerCase(), "Admin command: Handle the pvp team");
+        SubcommandData add = new SubcommandData("add", "Add a user to the pvp team manually");
+        OptionData raider = new OptionData(OptionType.USER, "user", "Mention of the user", true);
         OptionData name = new OptionData(OptionType.STRING, "name", "Character name", true);
         OptionData server = new OptionData(OptionType.STRING, "server", "Character server", false, true);
         OptionData role = new OptionData(OptionType.STRING, "role", "Character role", true, true);
         add.addOptions(raider,name,role,server);
-        SubcommandData remove = new SubcommandData("remove", "Remove a raider from the raid team manually");
+        SubcommandData remove = new SubcommandData("remove", "Remove a user from the pvp team manually");
         remove.addOptions(raider);
-        SubcommandData update = new SubcommandData("update", "Update the raid team message");
-        SubcommandData form = new SubcommandData("form", "Create a raid team application form");
+        SubcommandData update = new SubcommandData("update", "Update the pvp team message");
+        SubcommandData form = new SubcommandData("form", "Create a pvp team application form");
         command.addSubcommands(add,remove,update,form);
         return command;
     }
 
-    @Override
-    public boolean isSuccess() {
-        return success;
+    public Pvp(ButtonInteractionEvent event) {
+        super(event);
     }
-
-    public boolean isEphemeral(){
-        return shouldEphemeral;
+    public Pvp(ModalInteractionEvent event) {
+        super(event);
     }
-
-    @Override
-    public List<ActionRow> getActionRows() {
-        return actionRows;
-    }
-
-    @Override
-    public boolean shouldDefer() {
-        return defer;
-    }
-
-    public Raid(ButtonInteractionEvent event) {
-        defer = !event.getComponentId().split("::")[1].equals("apply");
-        shouldEphemeral = !event.getComponentId().split("::")[1].equals("apply");
-    }
-
-    public Raid(ModalInteractionEvent event){
-        defer = true;
-        shouldEphemeral = false;
-    }
-
-    public Raid(SlashCommandInteractionEvent event){
-        switch(event.getSubcommandName()){
-            case "add":
-            case "remove":
-            case "update":
-                shouldEphemeral = true;
-                break;
-            case "form":
-                shouldEphemeral = false;
-                break;
-        }
-    }
-
-    
-    public Object run(SlashCommandInteractionEvent event) {
-        Object result = "";
-        switch(event.getSubcommandName()){
-            case "add":
-                result = event.getOption("server") == null ? 
-                    add(event.getOption("raider").getAsUser(), event.getOption("name").getAsString(), event.getOption("role").getAsString(), "argent-dawn") :
-                    add(event.getOption("raider").getAsUser(), event.getOption("name").getAsString(), event.getOption("role").getAsString(), event.getOption("server").getAsString());
-
-                break;
-            case "remove":
-                result = remove(event.getOption("raider").getAsUser());
-                break;
-            case "update":
-                result = RaidTeam.update();
-                break;
-            case "form":
-                result = form();
-        }                
-        if(result == null){
-            success = false;
-            return "Command failed!";
-        }
-        return result;
-    }
-
-    private String add(User user, String name, String role, String server){
-        success = RaidTeam.add(user,name,role,server);
-        if(success){
-            return "Successfully added member to raid team!";
-        }
-        else{
-            return "Error, failed to add member to raid team!";
-        }
-    }
-
-    private String remove(User user){
-        RaidTeam.remove(user);
-        return "Successfully removed user from raid team!";
-    }
-
-
-    protected MessageEmbed form(){
-        EmbedBuilder builder = new EmbedBuilder();
-        String guildName = Bot.getConfig().get("guildName");
-        builder.setTitle("Apply to the "+this.getClass().getSimpleName().toLowerCase()+" team of "+guildName+"!");
-        builder.setDescription("Hi, here you can apply to the "+this.getClass().getSimpleName().toLowerCase()+" team!\nYou will receive a pop-up form to add your character's details.");
-        if(Bot.getConfig().get("guildImage") != null){
-            builder.setImage(Bot.getConfig().get("guildImage"));
-        }
-        actionRows.add(ActionRow.of(Button.primary(buildButtonId("apply", null), "Apply here!")));
-        return builder.build();
+    public Pvp(SlashCommandInteractionEvent event) {
+        super(event);
     }
 
     @Override
@@ -161,20 +70,16 @@ public class Raid implements Command {
                 TextInput role = TextInput.create("role", "Your role", TextInputStyle.SHORT)
                     .setPlaceholder("Healer, Tank, Ranged Damage or Melee Damage")
                     .build();
-                TextInput raidtimes = TextInput.create("raidtimes", "Wednesday and Sunday 19:30 - 22:30?", TextInputStyle.SHORT)
-                    .setPlaceholder("Can you raid with us? (yes/no)")
-                    .setValue("yes")
-                    .build();
     
                 Modal modal = Modal.create(buildSubId("modal", null), "Application form")
-                    .addActionRows(ActionRow.of(name), ActionRow.of(role), ActionRow.of(server), ActionRow.of(raidtimes))
+                    .addActionRows(ActionRow.of(name), ActionRow.of(role), ActionRow.of(server))
                     .build();
                 return modal;
             case "approve":
                 String[] data = event.getComponentId().split("::")[2].split(",");
-                RaidTeam.add(event.getUser(), data[0], data[2], data[1]);
+                PvpTeam.add(event.getUser(), data[0], data[2], data[1]);
                 event.getMessageChannel().deleteMessageById(event.getMessageId()).queue();
-                return "Successfully added raider: `"+data[0]+"` to raid team";
+                return "Successfully added user: `"+data[0]+"` to pvp team";
             case "decline":
                 String[] data1 = event.getComponentId().split("::")[2].split(",");
                 event.getMessageChannel().deleteMessageById(event.getMessageId()).queue();
@@ -182,15 +87,12 @@ public class Raid implements Command {
             default:
                 return "Error, invalid button identified by id: "+event.getComponentId();
         }
-
     }
-
     @Override
     public Object ModalExecute(ModalInteractionEvent event) {
         String name = event.getValue("name").getAsString();
         String server = event.getValue("server").getAsString().toLowerCase().replace(" ", "-");
         String role = event.getValue("role").getAsString();
-        boolean raidtimes = event.getValue("raidtimes").getAsString().equalsIgnoreCase("yes") ? true : false;
 
         if(!BattleNetAPI.verifyCharacter(name, server)){
             return "Error, this character is not valid - check the name or server, or check your battle.net account's security settings";
@@ -209,7 +111,6 @@ public class Raid implements Command {
         builder.appendDescription("\n**Specialization**: "+character.getSpecialization());
         builder.appendDescription("\n**Role**: "+Utils.capitalize(role));
         builder.appendDescription("\n**Item level**: "+character.getIlvl()+"/"+character.getAverageIlvl());
-        builder.appendDescription("\n**Availability**: "+ (raidtimes ? "yes" : "no"));
         builder.appendDescription("\n----------------------------");
         builder.setThumbnail(character.getAvatarURL());
 
@@ -227,12 +128,11 @@ public class Raid implements Command {
             else fields.add(new Field("We are not actively looking for:\n"+character._getClass(), "", true));
             if(needed.contains(role.toLowerCase())) score++;
             else fields.add(new Field("We do not need any more:\n"+role, "", true));
-            if(raidtimes) score++;
         }
         else fields.add(new Field("We do not need any more:\n"+role, "", false));
         
         for(Field field : fields) builder.addField(field);
-        builder.setColor(score >= 3 ? Color.GREEN : (score > 0 ? Color.YELLOW : Color.RED));
+        builder.setColor(score >= 2 ? Color.GREEN : (score > 0 ? Color.YELLOW : Color.RED));
 
         actionRows.add(ActionRow.of(
             Button.success(buildSubId("approve", name+","+server+","+role), "Approve"),
@@ -242,4 +142,41 @@ public class Raid implements Command {
         return builder.build();
     }
     
+    public Object run(SlashCommandInteractionEvent event) {
+        Object result = "";
+        switch(event.getSubcommandName()){
+            case "add":
+                result = event.getOption("server") == null ? 
+                    add(event.getOption("raider").getAsUser(), event.getOption("name").getAsString(), event.getOption("role").getAsString(), "argent-dawn") :
+                    add(event.getOption("raider").getAsUser(), event.getOption("name").getAsString(), event.getOption("role").getAsString(), event.getOption("server").getAsString());
+
+                break;
+            case "remove":
+                result = remove(event.getOption("raider").getAsUser());
+                break;
+            case "update":
+                result = PvpTeam.update();
+                break;
+            case "form":
+                result = form();
+        }                
+        if(result == null){
+            success = false;
+            return "Command failed!";
+        }
+        return result;
+    }
+    private String add(User user, String name, String role, String server){
+        success = PvpTeam.add(user,name,role,server);
+        if(success){
+            return "Successfully added member to raid team!";
+        }
+        else{
+            return "Error, failed to add member to raid team!";
+        }
+    }
+    private String remove(User user){
+        PvpTeam.remove(user);
+        return "Successfully removed user from raid team!";
+    }
 }
