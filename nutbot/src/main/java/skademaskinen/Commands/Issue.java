3685017a8017836e4ef26109dd01b54bd84448b8
mast3r_waf1z <thomasmollerjensen@live.dk>
2022-12-08 23:08:51 +0100

    implemented feature

---
+++ nutbot/src/main/java/skademaskinen/Commands/Issue.java
@@ -0,0 +1,53 @@
+package skademaskinen.Commands;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import skademaskinen.Bot;
+
+public class Issue implements Command{
+
+    public static CommandData configure(){
+        return Commands.slash(Issue.class.getSimpleName().toLowerCase(), "Write an issue report, this can be a feature request or a bug report etc.")
+            .addOption(OptionType.STRING, "issue", "The issue being reported", true)
+            .addOption(OptionType.STRING, "description", "The description of the issue", false);
+    }
+
+    private boolean success = false;
+
+    public Issue(SlashCommandInteractionEvent event) {
+    }
+
+    @Override
+    public boolean isSuccess() {
+        // TODO Auto-generated method stub
+        return success;
+    }
+
+    @Override
+    public Object run(SlashCommandInteractionEvent event) {
+        String title = event.getOption("issue").getAsString();
+        String description = event.getOption("description") == null ? "" : event.getOption("description").getAsString();
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle(title);
+        builder.setDescription(description);
+        builder.setAuthor(event.getMember().getEffectiveName(), null, event.getMember().getEffectiveAvatarUrl());
+        builder.setColor(event.getMember().getColor());
+        builder.setFooter(event.getGuild().getName());
+        if(Bot.getConfig().get("issues:channel") != null){
+            event.getGuild().getTextChannelById(Bot.getConfig().get("issues:channel")).sendMessageEmbeds(builder.build()).queue();
+            success = true;
+            return "Sent issue report!";
+        }
+        else return "Failed to send issue report! bot is not configured correctly by administrators!";
+    }
+
+    @Override
+    public boolean requiresAdmin() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+}
