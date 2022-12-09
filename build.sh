674b9b12c8072046b82120af11e265145481c4f6
Mast3r_waf1z <thomasmollerjensen@live.dk>
2022-12-09 11:31:23 +0100

    added verbose build option and hidden verbose stuff otherwise

--- build.sh
+++ build.sh
@@ -5,21 +5,22 @@ fi
 
 mkdir -p $verbose files
 if test -f "files/config.conf"; then
-    if [ $verbose == "-v" ]; then
+    if [ "$verbose" == "-v" ]; then
     	echo "file 'files/config.conf' already exists"
     fi
 else
-    if [ $verbose == "-v" ]; then
+    if [ "$verbose" == "-v" ]; then
     	echo "initializing file 'files/config.conf'"
     fi
     echo "token=" >> files/config.conf
 fi
-if [ $verbose == "-v" ]; then
+
+if [ "$verbose" == "-v" ]; then
     echo "compiling program..."
 fi
 
-rm *.jar $verbose
-if [ $verbose == "-v" ]; then
+rm *.jar -f $verbose
+if [ "$verbose" == "-v" ]; then
     mvn clean compile package -f nutbot
 else
     mvn clean compile package -q -f nutbot
