89d4d81460a7f9ac1f9179706e15e11f85fd3603
Thomas Jensen <mast3r@skade.dev>
2025-03-04 19:18:10 +0100

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Java Discord bot system modules";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
